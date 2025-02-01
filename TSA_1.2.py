1ae8353f31eec4fbd5a88c41c2bb528ac6ce1da5
TomasJr29 <tomjackpratboss@gmail.com>
2025-02-02 07:20:29 +0800

    Add files via upload

---
+++ TSA_1.2.py
@@ -0,0 +1,25 @@
+inp = input("Please enter a String: ")
+
+new_inp = inp.replace(" ", "")
+
+only_others = ""
+
+only_numbers = ""
+
+total_sum = 0
+
+for i in new_inp: 
+    
+    if (i.isdigit()):
+        only_numbers += i
+        
+    else:
+        only_others = i.replace(i, "")
+        
+print("Sum of the digits in the string: " + only_numbers)
+
+for i in only_numbers:
+    
+    total_sum += int(i)
+    
+print("Sum of the digits in the string: " + str(total_sum))
